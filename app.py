73d2f52ca31323fa05c8d4e9ff18ec45d01608ee
Marmoris <laharibandaru@gmail.com>
2022-08-12 11:01:06 -0400

    blah

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-    dropDownText = db.Column(db.Text)
+    dropDownText = db.Column(db.Text())
     totalMoney = db.Column(db.REAL, nullable=False)
 
 class BudgetChange(db.Model):
